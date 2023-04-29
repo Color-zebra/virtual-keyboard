a601526c49babf62e6c1c79cf23ca58b551cccda
Romanenkov Dmitriy <215rd@mail.ru>
2023-04-29 21:21:20 +0300

    feat: add styles for keyboard

--- modules/keyFactory.js
+++ modules/keyFactory.js
@@ -2,13 +2,16 @@ class KeyFactory {
   constructor() {
     this.createKeyElem = function createElem(tag, classNames, id, content) {
       const elem = document.createElement(tag);
+      const elemBack = document.createElement(tag);
+      elemBack.classList.add('back');
+      elem.append(elemBack);
       classNames.forEach((item) => elem.classList.add(item));
       if (id) elem.setAttribute('id', id);
       if (!content) return elem;
       if (typeof content === 'string') {
-        elem.innerText = content;
+        elemBack.innerText = content;
       } else {
-        elem.append(content);
+        elemBack.append(content);
       }
 
       return elem;
